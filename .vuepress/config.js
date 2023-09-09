8385e29d6df54d6c059387da379b498473e570ab
zhangji <1453411403@qq.com>
2023-09-09 17:58:18 +0800

    feat: monorepo开发记录

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -26,6 +26,7 @@ export default defineUserConfig({
                 'link': '/',
             },
             'next-full-stack-app.md',
+            'monorepo.md'
         ],
     }),
     plugins: [],
